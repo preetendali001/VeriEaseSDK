36de468b8cdc5d82a1c77639c20dd62d4b48399e
Preeten Dali <preeten@softmonks.com>
2024-12-27 10:09:00 +0530

    Update CameraPreview.swift

--- Sources/VeriEaseSDK/CameraPreview.swift
+++ Sources/VeriEaseSDK/CameraPreview.swift
@@ -8,22 +8,21 @@
 import SwiftUI
 import AVFoundation
 
-public struct CameraPreview: UIViewControllerRepresentable { // Make it public
-    public let previewLayer: AVCaptureVideoPreviewLayer // Make property public
+public struct CameraPreview: UIViewControllerRepresentable {
+    public let previewLayer: AVCaptureVideoPreviewLayer
 
-    // Public initializer to allow external initialization
     public init(previewLayer: AVCaptureVideoPreviewLayer) {
         self.previewLayer = previewLayer
     }
 
-    public func makeUIViewController(context: Context) -> UIViewController { // Make public
+    public func makeUIViewController(context: Context) -> UIViewController {
         let controller = UIViewController()
         controller.view.layer.addSublayer(previewLayer)
         previewLayer.frame = controller.view.bounds
         return controller
     }
 
-    public func updateUIViewController(_ uiViewController: UIViewController, context: Context) { // Make public
+    public func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
         previewLayer.frame = uiViewController.view.bounds
     }
 }
